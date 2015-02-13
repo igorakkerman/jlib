6f58d9b69589194b0cea9b4622af90a697add72b
Igor Akkerman <igor.akkerman@dynamite-services.de>
2015-02-13 12:51:10 +0100

    Base entity classes simplified

--- jlib-persistence-jpa/src/main/java/org/jlib/persistence/jpa/ApplicationJpaEntity.java
+++ jlib-persistence-jpa/src/main/java/org/jlib/persistence/jpa/ApplicationJpaEntity.java
@@ -1,33 +1,21 @@
-/*
- * jlib - Open Source Java Library
- *
- *     www.jlib.org
- *
- *
- *     Copyright 2005-2015 Igor Akkerman
- *
- *     Licensed under the Apache License, Version 2.0 (the "License");
- *     you may not use this file except in compliance with the License.
- *     You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
- */
-
 package org.jlib.persistence.jpa;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import static javax.persistence.GenerationType.AUTO;
+import static org.apache.commons.lang3.ArrayUtils.add;
 import org.jlib.basefunctions.ApplicationObject;
 import org.jlib.persistence.IdEntity;
 
 /**
- * JPA entity deriving from {@link ApplicationObject}.
+ * <p>
+ * Annotate the entity class with
+ * {@code @AttributeOverride(name = JpaEntity.FIELD_NAME_ID, column = @Column(name = "myentity_id"))}
+ * </p>
  *
  * @param <Self>
  *        own type
@@ -35,8 +23,24 @@ import org.jlib.persistence.IdEntity;
  * @param <ID>
  *        type of the id
  */
+@MappedSuperclass
 public abstract class ApplicationJpaEntity<Self extends ApplicationJpaEntity<Self, ID>, ID extends Serializable>
 extends ApplicationObject
-implements IdEntity<ID>{
-    // unifying ApplicationObject and IdEntity
+implements IdEntity<ID> {
+
+    public static final String FIELD_NAME_ID = "id";
+
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private ID id;
+
+    @Override
+    public ID getId() {
+        return id;
+    }
+
+    @Override
+    protected String[] getExcludedFieldNames() {
+        return add(super.getExcludedFieldNames(), "id");
+    }
 }
